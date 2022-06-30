18b46bbfdcc66f944ce5dbbde3eb4bbfe445c56a
Magdalena Sliwinska <madziakorczak7@gmail.com>
2022-06-30 22:17:48 +0200

    add styling and set up static directory

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 
 app.get('/', function (req, res) {
@@ -41,6 +42,6 @@ app.post("/register", function (req, res) {
 });
 
 
-app.listen(8080, function () {
-    console.log("App listening on port 8080 :)".bold.magenta);
+app.listen(3000, function () {
+    console.log("App listening on port 3000 :)".bold.magenta);
 });
